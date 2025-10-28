7921ce0bc19f05ae974a7089028a1d1da102921f
James Upjohn <jupjohn@jammeh.co.nz>
2025-10-28 14:30:02 +1300

    ref: move HTTP protocol configuration from source to config

--- src/MasayoshiDj/Program.cs
+++ src/MasayoshiDj/Program.cs
@@ -1,18 +1,7 @@
 using FastEndpoints;
 using MasayoshiDj.Features.Authentication;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.WebHost.ConfigureKestrel(options =>
-{
-    // TODO(jupjohn): don't hardcode this port
-    options.ListenAnyIP(7216, listenOptions =>
-    {
-        listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
-        listenOptions.UseHttps();
-    });
-});
 
 builder.AddApplicationAuthentication();
 
